dbd9a37ddf9e2092aaa06e8a31f5cc9607d16004
ikcetsok <panicz.kostek@gmail.com>
2022-05-13 08:50:19 +0200

    Create FunWithFunctions.js

---
+++ FunWithFunctions.js
@@ -0,0 +1,12 @@
+/*jshint esversion:6*/
+
+let arr=[1,2,3,4,5,6];
+//removeIndex element
+let removeIndex = 2;
+let before=arr.splice(0,removeIndex-1);
+console.log(arr);
+let after=arr.splice(1)
+console.log(before);
+console.log(after);
+arr=before.concat(after);
+console.log(arr);
