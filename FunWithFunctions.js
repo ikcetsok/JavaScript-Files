b44769c04435d7058d647c82f19815c89be16bae
ikcetsok <panicz.kostek@gmail.com>
2022-05-16 09:01:27 +0200

    Update FunWithFunctions.js

--- FunWithFunctions.js
+++ FunWithFunctions.js
@@ -11,11 +11,36 @@
 // arr=before.concat(after);
 // console.log(arr);
 
-const users = [
-  { name: 'John', age: 34 },
-  { name: 'Amy', age: 20 },
-  { name: 'camperCat', age: 10 }
+const users = [{
+    name: 'John',
+    age: 34
+  },
+  {
+    name: 'Amy',
+    age: 20
+  },
+  {
+    name: 'camperCat',
+    age: 10
+  }
 ];
 
 const names = users.map(user => user.name);
 console.log(names);
+
+// The global variable
+const s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+  const newArray = [];
+  // Only change code below this line
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i]));
+  };
+  // Only change code above this line
+  return newArray;
+};
+
+const new_s = s.myMap(function(item) {
+  return item * 2;
+});
