c6c01ac954fe3bbe85b95546cc4a2946bf0880f3
ikcetsok <panicz.kostek@gmail.com>
2022-05-19 14:25:24 +0200

    Update splitify.js

--- splitify.js
+++ splitify.js
@@ -7,3 +7,15 @@ function splitify(str) {
 }
 
 console.log(splitify("Hello World,I-am code"));
+
+function sentensify(str) {
+  // Only change code below this line
+  let split = str.split(/\W/);
+  console.log(split);
+  let joiner = split.join(" ");
+  return joiner;
+
+  // Only change code above this line
+}
+
+sentensify("May-the-force-be-with-you");
