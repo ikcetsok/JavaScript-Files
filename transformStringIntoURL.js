5c5c8407114c31afefc8473db1c68430f35362a5
ikcetsok <panicz.kostek@gmail.com>
2022-05-20 08:30:37 +0200

    2 new

---
+++ transformStringIntoURL.js
@@ -0,0 +1,18 @@
+// Only change code below this line
+function urlSlug(title) {
+  let tArr = title.split(' ');
+  let cleaned = tArr.filter(word => word != '');
+  console.log(cleaned);
+  cleaned = cleaned.map((word) => {
+    return word.toLowerCase();
+  });
+
+  let url = cleaned.reduce((acc, title) => `${acc}-${title}`, '');
+  let out = url.slice(1, url.length);
+  console.log(out);
+  return out;
+
+}
+
+// Only change code above this line
+urlSlug(" Winter Is  Coming");
